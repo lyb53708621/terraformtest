c096ef1fb23eba94a84b8ae5cf40524f6ce73c5e
Edwin Lu <yibolu@microsoft.com>
2022-12-15 16:59:46 +0800

    add backend file

---
+++ backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name = "terraform_test"
+    storage_account_name = "tfstatefileyibo"
+    container_name = "tfstatefilecontainer"
+    key = "dev.terrasform.tfstate"
+  }
+}
